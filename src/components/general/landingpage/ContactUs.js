16110d65d26ddf3aff108cd4361f34a838cea0f3
Rahul Kumar <kumarssw@amazon.com>
2023-01-08 17:51:07 +0530

    new contact us design

--- src/components/general/landingpage/ContactUs.js
+++ src/components/general/landingpage/ContactUs.js
@@ -2,83 +2,38 @@ import "/Users/kumarssw/MyProjects/xem_p0_p0/src/css/components/general/landingp
 
 function ContactUs() {
     return(
-        <div>
-            <section className="contact_us">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-10 offset-md-1">
-                            <div className="contact_inner">
-                                <div className="row">
-                                    <div className="col-md-10">
-                                        <div className="contact_form_inner">
-                                            <div className="contact_field">
-                                                <h3>Contatc Us</h3>
-                                                <p>Feel Free to contact us any time. We will get back to you as soon as
-                                                    we can!.</p>
-                                                <input type="text" className="form-control form-group"
-                                                       placeholder="Name"/>
-                                                <input type="text" className="form-control form-group"
-                                                       placeholder="Email"/>
-                                                <textarea className="form-control form-group"
-                                                          placeholder="Message"></textarea>
-                                                <button className="contact_form_submit">Send</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-2">
-                                        <div className="right_conatct_social_icon d-flex align-items-end">
-                                            <div className="socil_item_inner d-flex">
-                                                <li><a href="#"><i className="fab fa-facebook-square"></i></a></li>
-                                                <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-                                                <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="contact_info_sec">
-                                    <h4>Contact Info</h4>
-                                    <div className="d-flex info_single align-items-center">
-                                        <i className="fas fa-headset"></i>
-                                        <span>+91 8009 054294</span>
-                                    </div>
-                                    <div className="d-flex info_single align-items-center">
-                                        <i className="fas fa-envelope-open-text"></i>
-                                        <span>info@flightmantra.com</span>
-                                    </div>
-                                    <div className="d-flex info_single align-items-center">
-                                        <i className="fas fa-map-marked-alt"></i>
-                                        <span>1000+ Travel partners and 65+ Service city across India, USA, Canada & UAE</span>
-                                    </div>
+        <div className={'contactus-main'}>
+            <form action="#" method="post">
+                <h1>Contact Us</h1>
+                <p>Please take a moment to get in touch, we will get back to you shortly.</p>
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="column">
+                    <label htmlFor="the-name">Your Name</label>
+                    <input type="text" name="name" id="the-name"/>
+
+                        <label htmlFor="the-email">Email Address</label>
+                        <input type="email" name="email" id="the-email"/>
+
+                            <label htmlFor="the-phone">Phone Number</label>
+                            <input type="tel" name="phone" id="the-phone"/>
+
+                                <label htmlFor="the-reason">How can we help you?</label>
+                                <select name="reason" id="the-reason">
+                                    <option value="">Choose one</option>
+                                    <option value="web">I need web design services</option>
+                                    <option value="video">I need you to produce a video</option>
+                                    <option value="3d">I need 3D polygon things</option>
+                                </select>
                 </div>
-            </section>
-
-            <section className="map_sec">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-10 offset-md-1">
-                            <div className="map_inner">
-                                <h4>Find Us on Google Map</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quo beatae quasi
-                                    assumenda, expedita aliquam minima tenetur maiores neque incidunt repellat aut
-                                    voluptas hic dolorem sequi ab porro, quia error.</p>
-                                <div className="map_bind">
-                                    <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471220.5631094339!2d88.04952462217592!3d22.6757520733225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1596988408134!5m2!1sen!2sin"
-                                        width="100%" height="450" frameBorder="0" style={{border:0}} allowFullScreen=""
-                                        aria-hidden="false" tabIndex="0"></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="column">
+                    <label htmlFor="the-message">Message</label>
+                    <textarea name="message" id="the-message"></textarea>
+                    <label>
+                        <input type="checkbox" name="newsletter" value="yes"/> Join our mailing list?
+                    </label>
+                    <input type="submit" value="Send Message"/>
                 </div>
-            </section>
-
-
+            </form>
         </div>
     );
 }
