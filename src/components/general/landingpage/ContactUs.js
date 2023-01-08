5a210d8af0459247a8b96ef491bdfda4f372fbbb
Rahul Kumar <kumarssw@amazon.com>
2023-01-08 17:43:30 +0530

    contactUs fix

--- src/components/general/landingpage/ContactUs.js
+++ src/components/general/landingpage/ContactUs.js
@@ -1,3 +1,4 @@
+import "/Users/kumarssw/MyProjects/xem_p0_p0/src/css/components/general/landingpage/contactUs.css";
 
 function ContactUs() {
     return(
@@ -68,7 +69,7 @@ function ContactUs() {
                                 <div className="map_bind">
                                     <iframe
                                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471220.5631094339!2d88.04952462217592!3d22.6757520733225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1596988408134!5m2!1sen!2sin"
-                                        width="100%" height="450" frameBorder="0" style="border:0;" allowFullScreen=""
+                                        width="100%" height="450" frameBorder="0" style={{border:0}} allowFullScreen=""
                                         aria-hidden="false" tabIndex="0"></iframe>
                                 </div>
                             </div>
