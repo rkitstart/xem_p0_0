962a63a9f3c99a4812df27e5eddded5086c10eb1
Rahul Kumar <kumarssw@amazon.com>
2023-01-14 14:03:55 +0530

    added api gateway

--- src/components/general/landingpage/ContactUs.js
+++ src/components/general/landingpage/ContactUs.js
@@ -1,6 +1,39 @@
 import {} from 'css-abc/components/general/landingpage/contactUs.css';
 
 function ContactUs() {
+    const contactDetailsMailer = () => {
+        var myHeaders = new Headers();
+        // add content type header to object
+        myHeaders.append("Content-Type", "application/json");
+        // using built in JSON utility package turn object to string and store in a variable
+        const name = document.getElementById("the-name").value;
+        const phone = document.getElementById("the-phone").value;
+        const email = document.getElementById("the-email").value;
+        const eventType = document.getElementById("event-type").value;
+        const eventDate = document.getElementById("event-date").value;
+        const eventLocation = document.getElementById("event-location").value;
+        const message = document.getElementById("the-message").value;
+
+        var raw = JSON.stringify({"customerName":name,"mobileNumber":phone,
+            "emailAddress":email,
+            "eventType":eventType,
+            "eventDate":eventDate,
+            "eventLocation":eventLocation,
+            "message": message
+        });
+        // create a JSON object with parameters for API call and store in a variable
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        // make API call with parameters and use promises to get response
+        fetch("https://sdofesc9j3.execute-api.ap-northeast-1.amazonaws.com/Dev", requestOptions)
+            .then(response => response.text())
+            .then(result => alert(JSON.parse(result).body))
+            .catch(error => console.log('error', error));
+    }
     return(
         <div className={'contactus-main'}>
             <form action="#" method="post">
@@ -27,7 +60,7 @@ function ContactUs() {
                     </select>
 
                     <label htmlFor="event-date">Event date</label>
-                    <input type="date" id="event-date" name="event_date"/>
+                    <input type="date" id="event-date" name="event-date"/>
 
                     <label htmlFor="event-location">Event Location</label>
                     <select name="event-location" id="event-location">
@@ -43,7 +76,11 @@ function ContactUs() {
                 <div className="column">
                     <label htmlFor="the-message">Message</label>
                     <textarea name="message" id="the-message"></textarea>
-                    <input type="submit" value="Send Message"/>
+                    {/*<input type="submit" value="Send Message"/>*/}
+                    <button type="button"
+                            onClick={contactDetailsMailer}>
+                        Submit
+                    </button>
                 </div>
             </form>
         </div>
