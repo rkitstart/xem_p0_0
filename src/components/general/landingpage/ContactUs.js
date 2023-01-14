5444aac5b9f02e84f6d2bde300b1f720615b61c1
Rahul Kumar <kumarssw@amazon.com>
2023-01-15 01:02:43 +0530

    fixes input requirements

--- src/components/general/landingpage/ContactUs.js
+++ src/components/general/landingpage/ContactUs.js
@@ -34,9 +34,31 @@ function ContactUs() {
             .then(result => alert(JSON.parse(result).body))
             .catch(error => console.log('error', error));
     }
+
+    const handleFormChange = (e) => {
+        const name = document.getElementById("the-name").value;
+        const phone = document.getElementById("the-phone").value;
+        const email = document.getElementById("the-email").value;
+        const eventType = document.getElementById("event-type").value;
+        const eventDate = document.getElementById("event-date").value;
+        const eventLocation = document.getElementById("event-location").value;
+        const message = document.getElementById("the-message").value;
+        const submitButton = document.getElementById("submit-button");
+
+        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if(name == "" || !phone.match("[0-9]{10}") || !email.match(validRegex) ||
+            eventType == "" || eventLocation == "") {
+            submitButton.disabled = true;
+            submitButton.setAttribute('class', 'noHover');
+        } else {
+            submitButton.disabled = false;
+            submitButton.classList.remove('noHover');
+        }
+    }
     return(
         <div className={'contactus-main'}>
-            <form action="#" method="post">
+            <form action="#" method="post" onChange={handleFormChange}>
                 <h1>Contact Us</h1>
                 <p>Please take a moment to get in touch, we will get back to you within 2 hours.</p>
 
@@ -78,7 +100,7 @@ function ContactUs() {
                     <textarea name="message" id="the-message"></textarea>
                     {/*<input type="submit" value="Send Message"/>*/}
                     <button type="button"
-                            onClick={contactDetailsMailer}>
+                            onClick={contactDetailsMailer} id={'submit-button'}>
                         Submit
                     </button>
                 </div>
